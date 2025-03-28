a15560ea5af495a32f73dc2d939d70edd174987e
MartinTalan <martin.talan.377@gmail.com>
2025-03-28 16:35:15 +0100

    Added a test bash script which runs data_fetcher.py and is called by cron every minute.

--- apps/data_fetcher.py
+++ apps/data_fetcher.py
@@ -2,7 +2,7 @@
 """
 python script for continuously fetching data from Elia
 """
-
+import pandas as pd
 import requests
 import datetime
 
@@ -10,7 +10,7 @@ from src.data_download.data_download import quarter_hour_down_rounder
 
 current_time = datetime.datetime.now()
 current_time_UTC = current_time - datetime.timedelta(hours=1) # TODO: somehow account for annual summer time changes
-current_time_UTC_dalayed = current_time_UTC - datetime.timedelta(minutes=30) # time delay is cca 30 minutes
+current_time_UTC_dalayed = current_time_UTC - datetime.timedelta(minutes=15) # time delay is cca 30 minutes
 current_time_UTC_dalayed_rounded = quarter_hour_down_rounder(current_time_UTC_dalayed)
 
 selected_timeslot = current_time_UTC_dalayed_rounded.strftime("%Y-%m-%dT%H:%M:%S")
@@ -32,9 +32,16 @@ response = requests.get(API_URL,params=params)
 #response = requests.get(API_URL)
 
 data = response.json()
+#aa = data['results'][0]
 
-print(f"response status code: {response.status_code}")
-print(data)
+# creating a pandas df for the results
+fetched_entry = pd.DataFrame(data = data['results'][0], index=[0])
+col_names = ["datetime","offshoreonshore","realtime","monitoredcapacity"]#print(f"response status code: {response.status_code}")
+fetched_entry = fetched_entry.reindex(columns=col_names)
+
+#print(data)
+print(f"current UTC time: {current_time_UTC}")
+print(fetched_entry)
 
 
 #print("hello there my beautiful")
---
+++ bash_scripts/test_data_fetcher_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# a test bash script to see the basic functionality with cron
+
+#obtaining PATH variable to the parent folder
+SCRIPT=$(realpath "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+PROJECT_PATH="$(dirname "$SCRIPT_PATH")"
+#echo "$SCRIPT_PATH"
+#echo "$PROJECT_PATH"
+
+# executing test python script
+# this script goes to the project directory, activates venv, and runs the .py script
+cd "$PROJECT_PATH" || { echo "Directory not found"; exit 1; }
+. "$PROJECT_PATH"/venv/bin/activate # works with ".", does not work with "source"
+python3.11 -m apps.data_fetcher
+
+
